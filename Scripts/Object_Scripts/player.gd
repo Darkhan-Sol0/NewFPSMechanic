72cbdcbeb6749e8e88e7cdd1ac82af9adf667b9d
DarkhanSol0 <darkhansol069@gmail.com>
2023-07-20 12:15:00 +0900

    add double jump

--- Scripts/Object_Scripts/player.gd
+++ Scripts/Object_Scripts/player.gd
@@ -2,8 +2,11 @@ extends CharacterBody3D
 
 @export var attack_component : Attack_Component
 
-@export var speed : int = 6.0
-@export var jump : int = 4.5
+@export var speed : float = 6.0
+@export var jump : float = 4.5
+
+var speed_stop : float
+var can_jump : bool = true
 
 
 func _input(event):
@@ -17,10 +20,16 @@ func get_gravity(delta):
 	var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		speed_stop = 0.01
+	else:
+		can_jump = true
+		speed_stop = speed
 
 func get_movement():
-	if Input.is_action_just_pressed("space") and is_on_floor():
+	if Input.is_action_just_pressed("space") and can_jump:
 		velocity.y = jump
+		if not is_on_floor():
+			can_jump = false
 
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -28,8 +37,8 @@ func get_movement():
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, speed_stop)
+		velocity.z = move_toward(velocity.z, 0, speed_stop)
 
 func get_input():
 	attack_component.fire()
